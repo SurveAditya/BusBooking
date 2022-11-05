47458c656c123121d9ffa079a570f5db661c1989
ssaditya2002 <ssaditya2002@gmail.com>
2022-11-05 08:06:43 +0530

    Deployed

--- server.js
+++ server.js
@@ -14,5 +14,14 @@ app.use("/api/buses", busesRoute);
 app.use("/api/bookings", bookingsRoute);
 app.use("/api/locationRoute", locationRoute);
 
+if(process.env.NODE_ENV === "production")
+{
+    app.use(express.static("client/build"));
+  
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
+    });
+}
+
 
 app.listen(port,() =>console.log(`Node server listening on port ${port}`));
